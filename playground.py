01349b99b45e0cde1767e00a506abeb4adfea4ad
Kingcode <136673119+o2real@users.noreply.github.com>
2024-08-06 15:32:47 +0900

    args

---
+++ playground.py
@@ -0,0 +1,29 @@
+def add(*args):
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+
+print(add(3, 5, 6))
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3,multiply=5)
+
+class Car:
+
+    def __init__(self,**kw):
+        self.make = kw["make"]
+        self.model = kw["model"]
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="nissan", model="Gt-R")
+print(my_car.model)
