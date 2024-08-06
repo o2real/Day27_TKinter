a63eb2842b9d5478cb2d69c4d99cb3c44775b2dd
Kingcode <136673119+o2real@users.noreply.github.com>
2024-08-07 02:07:42 +0900

    button

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+
+#Button
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=f"{new_text} Button got clicked")
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+#Entry
+input = Entry(width=10)
+input.pack()
+print(input.get())
+
 
 
 
